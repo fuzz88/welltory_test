89362f573ac6bfc700b2888f7f5d4536698e419c
Ivan Oschepkov <ivan@oschepkov.ru>
2020-11-24 13:01:19 +0800

    update prettify.sh

--- prettify.sh
+++ prettify.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-for filename in ./data/event/*;
-
-do
-    python -m json.tool $filename >> $filename.tmp;
-    mv -f $filename.tmp $filename; 
-    echo $filename;
-
-done;
-
-for filename in ./data/schema/*;
+for filename in ./data/*/*;
 
 do
     python -m json.tool $filename >> $filename.tmp;
